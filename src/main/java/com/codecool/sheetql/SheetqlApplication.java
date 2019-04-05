76eff9449a48958d1d805dc8f00e0127255e8577
Adelaiten <karoltrzaska19@gmail.com>
2019-04-05 11:45:38 +0200

    refactored getting from google sheets to another class

--- src/main/java/com/codecool/sheetql/SheetqlApplication.java
+++ src/main/java/com/codecool/sheetql/SheetqlApplication.java
@@ -1,13 +1,15 @@
 package com.codecool.sheetql;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 @SpringBootApplication
 public class SheetqlApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SheetqlApplication.class, args);
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        List<List<Object>> values = new SheetsReader().getSheetLists("15NxaIMfWdj-0uIejGGWBSLPia2_iLN0TxJvKTxhjM_o", "Arkusz1!A1:B");
     }
-
 }
+
